bd04ae8660e824bfc392ff9c28e2fdbd113904ae
siirielli <siiri.lietu@gmail.com>
2025-05-11 17:06:57 +0200

    fix: position absolute to fixed

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,7 +5,7 @@ const CVUrl =
 
 export default function Nav() {
   return (
-    <div className="bg-primary-green absolute top-2 right-[var(--spacing-page-mobile)] z-50 rounded-full px-2 py-2 md:right-[var(--spacing-page)]">
+    <div className="bg-primary-green fixed top-2 right-[var(--spacing-page-mobile)] z-50 rounded-full px-2 py-2 md:right-[var(--spacing-page)]">
       <nav className="flex justify-end gap-4">
         <Button link="#contact" label="Contact" />
         <Button link={CVUrl} label="CV" />
